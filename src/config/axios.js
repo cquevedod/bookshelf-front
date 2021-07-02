ec8de02fb78c2d22fc807b19fd143ef6f627d49a
Carlos Quevedo <carlosquedid@gmail.com>
2021-07-02 03:02:52 -0500

    fix: default url set for review environments in heroku

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const clienteAxios = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: process.env.REACT_APP_BACKEND_URL ?? 'https://bookshelf-backendo-dev.herokuapp.com/'
 });
 
 export default clienteAxios;
