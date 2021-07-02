f1ccab746cf680de6fb2dbd22af528eca93273cc
Carlos Quevedo <carlosquedid@gmail.com>
2021-07-01 22:16:17 -0500

    feature: project initialized and pushed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import clienteAxios from './config/axios';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [consult, setConsult] = useState(true);
+
+  useEffect( () => {
+    if(consult) {
+      const consultarAPI = () => {
+        clienteAxios.get('/api/books')
+          .then(respuesta => {
+            setBooks(respuesta.data)
+            setConsult(false);
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+      consultarAPI();
+    }
+  }, [consult] );
+
+  const message = process.env.NODE_ENV === 'development'
+    ? <p>Develop </p>
+    : process.env.NODE_ENV === 'production'
+     ? <p>Production</p>
+     : <p>Test</p>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {message}
+        <p>{JSON.stringify(books)}</p>
       </header>
     </div>
   );
